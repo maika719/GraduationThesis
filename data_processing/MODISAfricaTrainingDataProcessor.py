df9534615b6fd7321fc8134c7a029e5018b3ab67
maika719 <maika.y719@gmail.com>
2025-02-15 16:21:59 +0900

    Update MODISAfricaTrainingDataProcessor.py

--- data_processing/MODISAfricaTrainingDataProcessor.py
+++ data_processing/MODISAfricaTrainingDataProcessor.py
@@ -15,7 +15,7 @@ from datetime import datetime
 
 # Define base paths for CSV files and result storage
 csv_base_path = "/home/maika/anaconda3-lab/project/sotsuken/data/Satellite Data(2024_ZhangMaoquan)/africa data/split_by_date"
-result_base_folder = './images/africa/0130/train/MODIS/' + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+result_base_folder = './images/africa/train/MODIS/' + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
 # Function to load CSV data
 def load_data(csv_file_path):
