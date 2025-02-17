6ef1dc0922eaae76fe8c1465658f640c318e8afa
maika719 <maika.y719@gmail.com>
2025-02-17 10:28:47 +0900

    Update MODISAfricaValidationDataProcessor.py

--- data_processing/MODISAfricaValidationDataProcessor.py
+++ data_processing/MODISAfricaValidationDataProcessor.py
@@ -16,7 +16,7 @@ from datetime import datetime
 import calendar
 
 # Define base paths for CSV files and result storage
-csv_base_path = "/home/maika/anaconda3-lab/project/sotsuken/data/Satellite Data(2024_ZhangMaoquan)/africa data/valid/split_by_date"
+csv_base_path = ""
 result_base_folder = './images/africa/valid/MODIS/' + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
 # Function to load CSV data
